f36b5439fd6ab8da75b5c8ec56c2bb1edd0536b7
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-08-26 15:21:53 +0200

    prepare to deploy v1

--- src/main/java/com/messenger/assetservice/AssetServiceApplication.java
+++ src/main/java/com/messenger/assetservice/AssetServiceApplication.java
@@ -3,9 +3,11 @@ package com.messenger.assetservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
+@EnableDiscoveryClient
 public class AssetServiceApplication {
 
     public static void main(String[] args) {
